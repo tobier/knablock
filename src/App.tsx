c1cf922b31d77c9c83e25f4dae8f4a49d11cfc34
Tobias Eriksson <162911+tobier@users.noreply.github.com>
2024-09-15 19:40:52 +0200

    Add flowbite

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import type { Component } from "solid-js";
+import "flowbite";
 
-import logo from "./logo.svg";
 import styles from "./App.module.css";
 
 const App: Component = () => {
   return (
     <div class={styles.App}>
       <header class={styles.header}>
-        <p>knäblock</p>
+        <p>
+          Edit <code>src/App.jsx</code> and save to reload.
+        </p>
       </header>
     </div>
   );
